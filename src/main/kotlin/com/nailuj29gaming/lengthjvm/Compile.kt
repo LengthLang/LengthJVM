33fdfa4b2fa137d48f77f836bebaa39a9c69435d
nailuj29gaming <54189157+nailuj29gaming@users.noreply.github.com>
2021-01-13 18:22:48 -0500

    try to fix the verify error

--- src/main/kotlin/com/nailuj29gaming/lengthjvm/Compile.kt
+++ src/main/kotlin/com/nailuj29gaming/lengthjvm/Compile.kt
@@ -1,10 +1,13 @@
 package com.nailuj29gaming.lengthjvm
 
-import org.objectweb.asm.ClassReader
-import org.objectweb.asm.ClassWriter
-import org.objectweb.asm.Opcodes
+import com.sun.org.apache.bcel.internal.generic.ALOAD
+import com.sun.org.apache.bcel.internal.generic.ICONST
+import jdk.nashorn.internal.runtime.regexp.joni.constants.OPCode
+import org.objectweb.asm.*
+
 import java.io.FileOutputStream
 import java.io.IOException
+import java.util.*
 
 fun compile(program: List<Parser.Op>, name: String) {
    val cw = ClassWriter(ClassWriter.COMPUTE_MAXS)
@@ -16,6 +19,7 @@ fun compile(program: List<Parser.Op>, name: String) {
       "java/lang/Object",
       null
    )
+   cw.visitSource("$name.len", null)
 
    val mv = cw.visitMethod(
       Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC,
@@ -25,21 +29,90 @@ fun compile(program: List<Parser.Op>, name: String) {
    )
 
    mv.visitCode()
+
+   mv.visitTypeInsn(Opcodes.NEW, "java/util/Scanner")
+   mv.visitInsn(Opcodes.DUP)
    mv.visitFieldInsn(
       Opcodes.GETSTATIC,
       "java/lang/System",
-      "out",
-      "Ljava/io/PrintStream;"
+      "in",
+      "Ljava/io/InputStream;"
    )
-   mv.visitLdcInsn("Hello, World")
    mv.visitMethodInsn(
-      Opcodes.INVOKEVIRTUAL,
-      "java/io/PrintStream",
-      "println",
-      "(Ljava/lang/String;)V",
+      Opcodes.INVOKESPECIAL,
+      "java/util/Scanner",
+      "<init>",
+      "(Ljava/io/InputStream;)V",
       false
    )
+   mv.visitVarInsn(Opcodes.ASTORE, 1)
+   val labels = mutableListOf<Label>()
+   for (i in program) {
+      val label = Label()
+      labels.add(label)
+   }
+   labels.add(Label())
+   program.forEachIndexed { i, instruction ->
+      val label = labels[i]
+      mv.visitLabel(label)
+      mv.visitLineNumber(i + 1, label)
+      when (instruction) {
+         Parser.Op.Input -> {
+            mv.visitVarInsn(Opcodes.ALOAD, 1)
+            mv.visitMethodInsn(
+               Opcodes.INVOKEVIRTUAL,
+               "java/util/Scanner",
+               "next",
+               "()Ljava/lang/String;",
+               false
+            )
+            mv.visitInsn(Opcodes.ICONST_0)
+            mv.visitMethodInsn(
+               Opcodes.INVOKEVIRTUAL,
+               "java/lang/String",
+               "charAt",
+               "(I)C",
+               false
+            )
+         }
+         Parser.Op.Add -> {
+            mv.visitInsn(Opcodes.IADD)
+         }
+         Parser.Op.Sub -> {
+            mv.visitInsn(Opcodes.ISUB)
+         }
+         Parser.Op.Cond -> {
+            mv.visitJumpInsn(Opcodes.IFEQ, labels[i+2])
+         }
+         Parser.Op.Dup -> {
+            mv.visitInsn(Opcodes.DUP)
+         }
+         Parser.Op.OutN -> {
+            mv.visitFieldInsn(
+               Opcodes.GETSTATIC,
+               "java/lang/System",
+               "out",
+               "Ljava/io/PrintStream;"
+            )
+            mv.visitInsn(Opcodes.SWAP)
+            mv.visitMethodInsn(
+               Opcodes.INVOKEVIRTUAL,
+               "java/io/PrintStream",
+               "print",
+               "(I)V",
+               false
+            )
+         }
+         else -> {
+            if (instruction is Parser.Op.Push) {
+               mv.visitLdcInsn(instruction.value)
+            }
+         }
+      }
+   }
+   mv.visitLabel(labels.last())
    mv.visitInsn(Opcodes.RETURN)
+
    mv.visitMaxs(0, 0)
    mv.visitEnd()
 
@@ -53,3 +126,4 @@ fun compile(program: List<Parser.Op>, name: String) {
       ex.printStackTrace()
    }
 }
+
--- src/main/kotlin/com/nailuj29gaming/lengthjvm/Main.kt
+++ src/main/kotlin/com/nailuj29gaming/lengthjvm/Main.kt
@@ -1,5 +1,12 @@
 package com.nailuj29gaming.lengthjvm
 
 fun main(args: Array<String>) {
-    compile(listOf(), "Hello")
+    compile(listOf(
+        Parser.Op.Input,
+        Parser.Op.Push(0x30),
+        Parser.Op.Sub,
+        Parser.Op.Cond,
+        Parser.Op.OutN,
+        Parser.Op.Push(0)
+    ), "Hello")
 }
